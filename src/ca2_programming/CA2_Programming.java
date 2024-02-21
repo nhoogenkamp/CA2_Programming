7c2876183d602ea43d0e8ff66a405c22fd6fbc30
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2024-02-21 09:10:58 +0000

    Initial commit

---
+++ src/ca2_programming/CA2_Programming.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca2_programming;
+
+/**
+ *
+ * @author Capitania
+ */
+public class CA2_Programming {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
