a42aa37f94a6dc0dc6e2f5007d0f0e134affc353
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2024-02-22 12:56:22 +0000

    Company Class
    Added a new class: company, added a field CompanyName and an arrraylist.
    made a default constructor with the company name Buckley and a overloaded one.

---
+++ src/ca2_programming/Company.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca2_programming;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Capitania
+ */
+public class Company {
+    CA2_Programming program = new CA2_Programming();
+    private String companyName;
+    private ArrayList <CA2_Programming.employee> staff;
+    
+    public Company () {
+        this.companyName = "Buckley";
+        this.staff = new ArrayList();     
+    }
+        
+    public Company (String companyName) {
+        this.companyName = companyName;
+        this.staff = new ArrayList();     
+    }       
+    
+    
+    
+    
+    
+    
+}
